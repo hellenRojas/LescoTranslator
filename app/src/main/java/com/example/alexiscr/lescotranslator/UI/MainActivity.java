56a445fbd98d5e1744f0d5904e80c7a3375031ba
Hellen Rojas <Hellen Rojas>
2016-07-18 15:29:59 -0500

    Inicializar proyecto

---
+++ app/src/main/java/com/example/alexiscr/lescotranslator/UI/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.alexiscr.lescotranslator.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.alexiscr.lescotranslator.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main );
+    }
+}
