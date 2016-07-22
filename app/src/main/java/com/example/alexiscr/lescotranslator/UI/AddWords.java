65c036237da18c022041d04ddf59461b9df23d77
Kimberly Morales <kimberly.morales.arias@gmail.com>
2016-07-22 12:41:54 -0600

    Imágenes guardadas en la base de datos.

--- app/src/main/java/com/example/alexiscr/lescotranslator/UI/AddWords.java
+++ app/src/main/java/com/example/alexiscr/lescotranslator/UI/AddWords.java
@@ -5,19 +5,31 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.alexiscr.lescotranslator.Logic.Core.ImageConverter;
+import com.example.alexiscr.lescotranslator.Logic.Core.LescoObject;
 import com.example.alexiscr.lescotranslator.R;
 
 import java.io.IOException;
+
+import io.realm.Realm;
 
 public class AddWords extends AppCompatActivity implements View.OnClickListener{
     private ImageButton addImage;
     private ImageView imgVPhoto;
     private Bitmap bitmap;
     private int PICK_IMAGE_REQUEST;
+    private EditText wordToadd;
+    private Button addWord;
+
+    Realm realm = Realm.getDefaultInstance();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,15 +37,43 @@ public class AddWords extends AppCompatActivity implements View.OnClickListener{
         setContentView(R.layout.activity_add_words);
         addImage = (ImageButton)findViewById(R.id.imageButtonAddWord);
         addImage.setOnClickListener(this);
+        addWord = (Button)findViewById(R.id.buttonAddDatabaseWord);
         imgVPhoto = (ImageView)findViewById(R.id.imageViewNewPhoto);
+        wordToadd = (EditText)findViewById(R.id.editTextWritteWord);
+        addWord.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
+        String photoName;
         switch (view.getId()){
             case R.id.imageButtonAddWord:
                 showFileChooser();
                 break;
+            case R.id.buttonAddDatabaseWord:
+                photoName= wordToadd.getText().toString();
+                if(photoName.equals("")){
+                    Toast.makeText(this,"Ingrese un nombre válido",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    try {
+                        LescoObject newLescoObject = new LescoObject(photoName, ImageConverter.bitmapToByteArray(bitmap));
+                        realm.beginTransaction();
+                        LescoObject newRealmLescoObject = realm.copyToRealm(newLescoObject);
+                        realm.commitTransaction();
+                    }
+                    catch(Exception ex){
+                        Log.e("Error", ex.getLocalizedMessage());
+                        realm.cancelTransaction();
+                    }
+                    wordToadd.setText("");
+                    imgVPhoto.setImageDrawable(null);
+                    imgVPhoto.setImageDrawable(null);
+                    Toast.makeText(this,"Nueva palabra agregada",Toast.LENGTH_SHORT).show();
+
+                }
+
         }
     }
 
